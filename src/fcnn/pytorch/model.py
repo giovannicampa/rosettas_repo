5031fb60b8db35da0233047c46583deaf8dc7485
giovannicampa <giocampa93@gmail.com>
2024-02-24 22:01:22 +0100

    Explicit latten in pytorch fcnn

--- src/fcnn/pytorch/model.py
+++ src/fcnn/pytorch/model.py
@@ -6,18 +6,15 @@ import torch.nn.functional as F
 class FeedforwardNeuralNet(nn.Module):
     def __init__(self, input_size, num_classes):
         super(FeedforwardNeuralNet, self).__init__()
-        # First fully connected layer
 
         self.flat_input_size = input_size[0] * input_size[1]
-        self.fc1 = nn.Linear(
-            self.flat_input_size, 256
-        )  # First layer takes the flattened image
+        self.fc1 = nn.Linear(self.flat_input_size, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, num_classes)
 
     def forward(self, x):
 
-        x = x.view(-1, 784)  # Flatten input image
+        x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
